e0b431128721a6f158494485f9323dcc9db60994
pietromogno <pietro.mogno98@gmail.com>
2018-02-22 19:19:30 +0100

    Consegna FlatDice

---
+++ FlatDice/app/src/main/java/com/example/pietro/flatdice/MioFragment.java
@@ -0,0 +1,110 @@
+package com.example.pietro.flatdice;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MioFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MioFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MioFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private String [] facceDado = {"\u2680","\u2681","\u2682","\u2683","\u2684","\u2685"};
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private OnFragmentInteractionListener mListener;
+
+    public MioFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment MioFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MioFragment newInstance(String param1) {
+
+        MioFragment fragment = new MioFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            super.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_mio, container, false);
+        ((TextView)v.findViewById(R.id.txt)).setText(facceDado[Integer.parseInt(mParam1)]);
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+
+    }
+}
